d7157f18b9c9dc19829f8cd04a464b95b1c2af9e
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-29 16:01:35 +0200

    helper function determin winner

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -25,7 +25,8 @@ class GameContainer extends React.Component {
             history: history.concat({
                 squares: squares
             }),
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
         })
     }
 
@@ -44,6 +45,29 @@ class GameContainer extends React.Component {
             </div>
         )
     }
+}
+
+//Helper functions
+const calculateWinner = squares => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
 }
 
 export default GameContainer;
