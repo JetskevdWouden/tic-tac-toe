1a7c6b8e1a312855c49f9b53105627460f377090
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-29 16:16:34 +0200

    game info div

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -13,12 +13,22 @@ class GameContainer extends React.Component {
         }
     }
 
+    jumpTo = (step) => {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
+
     handleClick = (index) => {
         //previsous state of history
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-
+        const winner = calculateWinner(squares);
+        if (winner || squares[index]) {
+            return;
+        }
         squares[index] = this.state.xIsNext ? 'X' : 'O';
 
         this.setState({
@@ -33,6 +43,23 @@ class GameContainer extends React.Component {
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move ? 'GO TO #' + move : 'START GAME';
+            return (
+                <li key={move}>
+                    <button onClick={() => { this.jumpTo(move) }}>
+                        {desc}
+                    </button>
+                </li>
+            )
+        });
+        let status;
+        if (winner) {
+            status = 'WINNER IS ' + winner;
+        } else {
+            status = 'NEXT PLAYER IS ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div className="game">
@@ -41,6 +68,10 @@ class GameContainer extends React.Component {
                         onClick={(index) => this.handleClick(index)}
                         squares={current.squares}
                     />
+                </div>
+                <div className="game-info">
+                    <div className="status">{status}</div>
+                    <ul>{moves}</ul>
                 </div>
             </div>
         )
